73c7d084579064d518e9da71e6e257ed232dd79d
JonSilvestrini <jonatas.silvestrini@gmail.com>
2022-09-08 13:10:14 -0300

    DB conn and config

---
+++ code/classes/Registro.class.php
@@ -0,0 +1,87 @@
+<?php
+
+class Registro {
+
+	private $data;
+	private $tabela;
+	private static $con;
+
+	public function __construct($tabela, PDO $con) {
+		$this->tabela = $tabela;
+		self::$con = $con;
+	}
+
+	public function __set($prop, $value) {
+		$this->data[$prop] = $value;
+	}
+
+	public function __get($prop) {
+		return $this->data[$prop];
+	}
+
+	public function save($id) {
+		if ($id == 0) {
+			$sql = 'insert into ' . $this->tabela . '(' . implode(',', array_keys($this->data)) . ') values' . "('" . implode("','", array_values($this->data)) . "')";
+		} else {
+			$conta = 1;
+			$qtde = sizeof($this->data);
+			foreach ($this->data as $key => $value) {
+				if ($conta == 1) {
+					$sql = "update " . $this->tabela . " set ";
+				} else if ($conta >= $qtde) {
+					$sql .= " $key = '$value'";
+				} else {
+					$sql .= " $key = '$value', ";
+				}
+				$conta++;
+			}
+			$sql .= " where codigo=$id";
+		}
+		try {
+			return self::$con->query($sql);
+			//echo $id;
+			//echo "$sql";
+		} catch (Exception $e) {
+			echo "Erro na operação<p> {$e->getMessage()}";
+			exit;
+		}
+		
+		
+	}
+	
+	public function findAll(){
+			$sql = "select * from " . $this->tabela;
+			$stmt= self::$con->prepare($sql);
+			$stmt->execute();
+			return $stmt->fetchAll();
+	}
+		
+	public function findCriterio($campos, $criterio) {
+		$sql = "select ". $campos . " FROM " . $this->tabela . " "  . $criterio;
+		//echo $sql;exit;
+		$stmt= self::$con->prepare($sql);
+		$stmt->execute();
+		return $stmt->fetchAll();
+	}
+	
+	public function delete($id){
+		$sql = "delete from " . $this->tabela . ' where codigo='.$id;
+		try {
+			return self::$con->query($sql);
+		} catch (Exception $e) {
+			echo "Erro na operação! <p> {$e->getMessage()}";exit;
+		}
+	}
+	
+	public function find($id=NULL){
+		if ($id) {
+			$sql = "select * from ". $this->tabela .' where codigo='.$id;
+			$stmt= self::$con->prepare($sql);
+			$stmt->execute();
+			return $stmt->fetch();
+		}else{
+			return array(array_keys($this->data));
+		}
+	}
+
+}
